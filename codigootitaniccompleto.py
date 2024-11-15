c88565bd6bbced7b1c670b9111c190161dff9a2a
xnto8 <asanchezm9@correo.uss.cl>
2024-11-14 23:25:33 -0300

    Update codigootitaniccompleto.py

--- codigootitaniccompleto.py
+++ codigootitaniccompleto.py
@@ -23,6 +23,8 @@ if uploaded_file is not None:
     x_axis=st.selectbox("selecciona el eje x", df.select_dtypes(include= "number").columns)
     y_axis=st.selectbox("selecciona el eje y", df.select_dtypes(include= "number").columns)
     chart = alt.Chart(df).mark_circle(size=60).encode(
-        x=x_axis, y=y_axis,tooltip=[x_axis, y_axis].interactive()
+        x=x_axis, 
+        y=y_axis,
+        tooltip=[x_axis, y_axis].interactive()
     st.altair_chart(chart, use_container_width=True))
 
